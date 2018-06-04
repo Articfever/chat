3e4339a85d0d26dd22052e58216c29adae0d8d7f
Articfever <rlathrop666@gmail.com>
2018-06-04 10:01:08 -0700

    messages2

--- my-chat/src/components/messages.js
+++ my-chat/src/components/messages.js
@@ -7,14 +7,14 @@ class MessageList extends Component {
     super(props);
     this.state = {username: "", content: "", sentAt: "", RoomId:"",messages:[] } ;
     // Create message link to firebase
-    this.messageRef = this.props.firebase.database().ref("messages/"+this.props.activeRoom);
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleContentChange = this.handleContentChange.bind(this);
     this.createMessage = this.createMessage.bind(this);
   }
 
   componentDidMount() {
-    this.messageRef.on('child_added', snapshot => {
+    const messageRef = this.props.firebase.database().ref("messages/");
+    messageRef.on('child_added', snapshot => {
     const message = snapshot.val();
     message.key = snapshot.key;
     this.setState({ messages: this.state.messages.concat(message) })
@@ -48,12 +48,13 @@ class MessageList extends Component {
 
 
  createMessage(e){
- e.preventDefault();
-  this.messageRef.push({
-   username: this.state.username,
-   content: this.state.content,
-   sentAt: this.state.sentAt,
-   RoomId: this.state.RoomId
+   e.preventDefault();
+   const messageRef = this.props.firebase.database().ref("messages/");
+    messageRef.push({
+    username: this.state.username,
+    content: this.state.content,
+    sentAt: this.state.sentAt,
+    RoomId: this.state.RoomId
  });
  this.setState({ username: "", content: "", sentAt: "",RoomId:""});
 }
@@ -62,25 +63,30 @@ class MessageList extends Component {
 render() {
   return (
     <section className="MessageList">
-      <form className="addMessage">
-      <input type="text" value={this.state.username} placeholder="Display Name"
-       style={{width: '300px'}} onChange={this.handleNameChange}/>
-        <input type="text" value={this.state.content} placeholder="Type message here"
-         style={{width: '300px', height:'200px'}} onChange={this.handleContentChange}/>
-        <button type="submit" onClick={this.createMessage}>Send</button>
-      </form>
-      <table className="MessageList">
-        <colgroup>
-          <col span="1"/>
-        </colgroup>
-        { this.state.messages.map( (message) =>
-           <tr key={message.key} className={message.RoomId===this.props.activeRoom?'messageShow':'messageNoShow'}>
-             <h4>{message.username}</h4>
-             <p>{message.content}</p>
-           </tr>
-        )}
-      </table>
-    </section>
+    <form className="addMessage">
+     <input type="text" value={this.state.username} placeholder="Display Name"
+      style={{width: '300px'}} onChange={this.handleNameChange}/>
+       <input type="text" value={this.state.content} placeholder="Type message here"
+        style={{width: '300px', height:'200px'}} onChange={this.handleContentChange}/>
+       <button type="submit" onClick={this.createMessage}>Send</button>
+     </form>
+     <table className="MessageList">
+       <colgroup>
+         <col span="1"/>
+       </colgroup>
+       { this.state.messages.map( (message) =>{
+       if(this.props.activeRoom===message.RoomId){
+           return (
+             <tr key={message.key}>
+              <h4>{message.username}</h4>
+              <p>{message.content}</p>
+           </tr>);
+         }
+           else {return null;}
+         })
+       }
+     </table>
+   </section>
 
    );
   }
